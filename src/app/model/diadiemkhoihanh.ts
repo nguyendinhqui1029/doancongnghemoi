e894621c230e6ba905d7ffe58d402be245a2f4e8
Hồng Quý Trương <hongquy0802@gmail.com>
2018-10-17 15:23:31 +0700

    quy17/10

--- src/app/model/diadiemkhoihanh.ts
+++ src/app/model/diadiemkhoihanh.ts
@@ -1,14 +1,16 @@
 export class Diadiemkhoihanh
 {
+    id_diadiem:any;
     OriginCode:any;
     OriginName:any;
     DestCode:any;
     DestName:any;
-   constructor( OriginCode,OriginName,DestCode,DestName)
+   constructor( id_diadiem,OriginCode,OriginName,DestCode,DestName)
    {
-     this.OriginCode=OriginCode,
-     this.OriginName=OriginName,
-     this.DestCode=DestCode,
-     this.DestName=DestName
+     this.id_diadiem= id_diadiem;
+     this.OriginCode=OriginCode;
+     this.OriginName=OriginName;
+     this.DestCode=DestCode;
+     this.DestName=DestName;
    }
 }
