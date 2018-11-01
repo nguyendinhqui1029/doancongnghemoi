cfc80679e58a619c590585a770e8374a05484efa
Hồng Quý Trương <hongquy0802@gmail.com>
2018-11-01 22:04:37 +0700

    Quy.1.11 Them chuyen xe vao mock_tuyenduong

---
+++ src/app/model/chuyenxe.ts
@@ -0,0 +1,9 @@
+export class ChuyenXe{
+    id_chuyenxe: any;
+    ghe_chuyenxe: any[]=[];
+    constructor(id_chuyenxe,ghe_chuyenxe)
+    {
+        this.id_chuyenxe = id_chuyenxe;
+        this.ghe_chuyenxe = ghe_chuyenxe;
+    }
+}
--- src/app/model/tuyenduong.ts
+++ src/app/model/tuyenduong.ts
@@ -1,4 +1,5 @@
 import { ChiTietTuyenDuong } from "./chitiettuyenduong";
+import { ChuyenXe } from "./chuyenxe";
 
 export class TuyenDuong{
     id_tuyenduong:any;
@@ -12,23 +13,12 @@ export class TuyenDuong{
     giave:any;
     giochay:any;
     chitiet:ChiTietTuyenDuong[]=[];
-    // diemdi:any;
-    // diemden:any;
-    // thoigiandi: any;
-    // thoigianden:any;
-    // hotlinedi:any;
-    // hotlineden:any;
-    // diachidi:any;
-    // diachiden:any;
-    // toadodi:any;
-    // toadoden:any;
+    OriginCode:any;
+    DestCode:any;
+    chuyenxe: ChuyenXe[]=[];
     constructor(id_tuyenduong,name_tuyenduong,bendi,benden,loaixe,quangduong,thoigian,sochuyen,giave,giochay,
-        chitiet)//,diemdi,diemden,thoigiandi,thoigianden,hotlinedi,hotlineden,diachidi,diachiden,toadodi,toadoden)
-    { // hay tạo 1 cái chi tiết riêng đê tao coi cái chắc nên tao thêm cái chi tiết rồi. Import cái chi tiết vô cái tuyến đường này đc k 
-        //được thay kiêu any[]thanh CHITIET[]
-        // ok đê lam thử
-
-
+        chitiet,OriginCode,DestCode,chuyenxe)
+    { 
        this.id_tuyenduong= id_tuyenduong;
        this.name_tuyenduong= name_tuyenduong;
        this.bendi= bendi;
@@ -40,17 +30,9 @@ export class TuyenDuong{
        this.giave= giave;
        this.giochay= giochay;
        this.chitiet = chitiet;
-    //    this.diemdi = diemdi;
-    //    this.diemden = diemden;
-    //    this.thoigiandi = thoigiandi;
-    //    this.thoigianden = thoigianden;
-    //    this.hotlinedi = hotlinedi;
-    //    this.hotlineden = hotlineden;
-    //    this.diachidi = diachidi;
-    //    this.diachiden = diachiden;
-    //    this.toadodi = toadodi;
-    //    this.toadoden = toadoden;
+       this.OriginCode = OriginCode;
+       this.DestCode = DestCode;
+       this.chuyenxe = chuyenxe;
 
-    
     }
 }
