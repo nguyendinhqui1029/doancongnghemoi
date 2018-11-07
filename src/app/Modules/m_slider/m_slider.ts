3842169a068732243f01db6c215b6f0090c0502d
Hồng Quý Trương <hongquy0802@gmail.com>
2018-11-07 14:47:58 +0700

    Quy7.11

--- src/app/Modules/m_slider/m_slider.ts
+++ src/app/Modules/m_slider/m_slider.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 
 @Component({
   selector: 'm_slider',
   templateUrl: './m_slider.html',
 })
-export class M_SLIDER implements OnInit  {
+export class M_SLIDER implements OnInit, OnDestroy  {
+  a:any="";
+
+  ngOnDestroy()
+   {
+    clearInterval(this.a);
+  }
     ngOnInit() {
     var s1 = 0;
-    s2();
+    this.a = s2();
     function s2() {
         var i;
         var slides = document.getElementsByClassName('slide123');
@@ -17,10 +23,12 @@ export class M_SLIDER implements OnInit  {
            (slides[i] as HTMLElement).style.display = "none";  
         }
         s1++;
-        if (s1 > slides.length) {s1 = 1;}    
+        if (s1 > slides.length) {
+          s1 = 1;
+        }    
         
         (slides[s1-1] as HTMLElement).style.display = "block";  
- 
+        
         setTimeout(s2, 2000); // Change image every 2 seconds
     }
 
